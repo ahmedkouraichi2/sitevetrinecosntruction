d5b2743df8da044291d7d1cbc98ef10fadc512dc
kouraichiahmed2 <kouraichiahmed2@gmail.com>
2023-10-19 13:52:00 +0200

    test

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,39 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+<script>
+  function sendEmail() {
+    emailjs.init("user_your_emailjs_user_id");
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    var templateParams = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value
+    };
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    emailjs.send("service_your_emailjs_service_id", "template_your_emailjs_template_id", templateParams)
+      .then(
+        function(response) {
+          // Succès - Affiche le message de succès
+          document.getElementById('contact-form').reset(); // Réinitialise le formulaire
+          showSuccessMessage();
+        },
+        function(error) {
+          // Erreur - Affiche le message d'erreur
+          showErrorMessage();
+        }
+      );
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  function showSuccessMessage() {
+    // Affiche le message de succès
+    document.querySelector('.loading').style.display = 'none';
+    document.querySelector('.error-message').style.display = 'none';
+    document.querySelector('.sent-message').style.display = 'block';
+  }
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+  function showErrorMessage() {
+    // Affiche le message d'erreur
+    document.querySelector('.loading').style.display = 'none';
+    document.querySelector('.sent-message').style.display = 'none';
+    document.querySelector('.error-message').style.display = 'block';
+  }
+</script>
